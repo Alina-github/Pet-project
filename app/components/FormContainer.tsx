f290b9aa56fa9f542945d10231ad211e576fb41d
Alina Rudkovskaya <arudkovskaya@demandbase.com>
2025-02-28 00:08:58 +0000

    Introduce HeroUI button

--- app/components/FormContainer.tsx
+++ app/components/FormContainer.tsx
@@ -7,15 +7,17 @@ interface FormContainerProps {
 
 const FormContainer = ({ title, children }: FormContainerProps) => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-50 p-4">
-            <Card className="w-full max-w-md shadow-lg">
-                <CardHeader className="flex justify-center pb-0">
-                    <h2 className="text-2xl font-bold text-center text-primary">{title}</h2>
-                </CardHeader>
-                <CardBody className="flex flex-col gap-4 px-6 py-4">{children}</CardBody>
-            </Card>
-        </div>
-    );
-};
+      <div className="h-screen flex justify-center items-center bg-gray-50 p-4">
+          <div className="w-1/2 flex justify-center items-center min-h-screen bg-gray-50 p-4">
+              <Card className="w-full max-w-md shadow-lg">
+                  <CardHeader className="flex justify-center pb-0">
+                      <h2 className="text-2xl font-bold text-center text-primary">{title}</h2>
+                  </CardHeader>
+                  <CardBody className="flex flex-col gap-4 px-6 py-4">{children}</CardBody>
+              </Card>
+          </div>
+      </div>
+          );
+          };
 
-export default FormContainer;
+          export default FormContainer;
